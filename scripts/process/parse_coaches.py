47a675a321c57b8d6ecfa0df4a3331248124dae4
Keltoi Conde Cabaleiro <keltoi.conde@quobis.com>
2026-04-14 10:18:43 +0200

    Many coach updates

--- scripts/process/parse_coaches.py
+++ scripts/process/parse_coaches.py
@@ -42,6 +42,11 @@ def parse_table(html_path: Path) -> list[dict]:
             continue
         
         name = cols[2].get_text(strip=True)
+
+        # Eliminar si el entrenador es un comité
+        if name.startswith("Nomination Committee"):
+            continue
+
         matches = int(cols[8].get_text(strip=True).replace(".", ""))
         if matches == 0:
             ppp = 0.0
@@ -90,54 +95,6 @@ def obtain_json(team: dict, processed_dir: Path):
         encoding="utf-8"
     )  
 
-def obtain_coach(team: dict):
-    raw_dir = Path("data/raw/transfermarkt/teams")
-
-    html_file = raw_dir / f"{slugify(team['name'])}_coaches.html"
-
-    if not html_file.exists():
-        print(f"No hay HTML para {team['name']}")
-        return
-
-    all_rows = []
-
-    rows = parse_table(html_file)
-    all_rows.extend(rows)
-
-    merged = merge_coaches(all_rows)
-
-    # Convertimos a lista y ordenamos por puntos, partidos, victorias y diferencia de goles
-    all_coaches = sorted(
-        merged.values(),
-        key=lambda x: (
-            x["matches"],
-            x["ppp"],
-        ),
-    reverse=True
-    )
-
-    top = all_coaches[0]
-    second = all_coaches[1]
-
-    if top["matches"] / second["matches"] >= 1.30:
-        return top
-    
-    subset = all_coaches[:5]
-
-    max_m = max(c["matches"] for c in subset)
-    max_ppp = max(c["ppp"] for c in subset)
-
-    def score(c):
-        m_norm = c["matches"] / max_m
-        ppp_norm = c["ppp"] / max_ppp
-
-        base = 0.70 * m_norm + 0.30 * ppp_norm
-
-        sample_factor = min(1, c["matches"] / 100)
-        return base * (0.85 + 0.15 * sample_factor)
-
-    return max(subset, key=score)
-
 def main():
     teams = yaml.safe_load(
         Path("config/teams.yml").read_text(encoding="utf-8")
@@ -147,9 +104,6 @@ def main():
     processed_dir.mkdir(parents=True, exist_ok=True)
 
     for team in teams:
-        #print(team["name"])
-        #coach = obtain_coach(team)
-        #team["coach"] = coach["name"] # type: ignore
         obtain_json(team, processed_dir)
 
 if __name__ == "__main__":
