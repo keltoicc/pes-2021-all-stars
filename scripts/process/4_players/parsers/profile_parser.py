8323b8902f54c7d2fad8809f8fe33b1c2ab4a42b
Keltoi Conde Cabaleiro <keltoi.conde@quobis.com>
2026-05-19 17:45:20 +0200

    Update scripts

--- scripts/process/4_players/parsers/profile_parser.py
+++ scripts/process/4_players/parsers/profile_parser.py
@@ -1,15 +1,21 @@
 import json
 
+from mappings.positions import POSITION_MAP
 
 def parse_profile(profile_path):
     with open(profile_path, "r", encoding="utf-8") as f:
         data = json.load(f)
 
     return {
-        "shortName": data.get("shortName"),
-        "date_of_birth": data.get("dateOfBirth"),
-        "place_of_birth": data.get("placeOfBirth"),
-        "height": data.get("height"),
-        "preferred_foot": data.get("foot"),
-        "nationality": data.get("nationality")
+        "short_name": data.get("data", [{}])[0].get("shortName"),
+        "nation_1": data.get("data", [{}])[0].get("nationalityDetails").get("nationalities").get("nationalityId"),
+        "nation_2": data.get("data", [{}])[0].get("nationalityDetails").get("nationalities").get("secondNationalityId"),
+        "height": data.get("data", [{}])[0].get("attributes").get("height"),
+        "position_group": data.get("data", [{}])[0].get("attributes").get("positionGroup"),
+        "position_1": POSITION_MAP.get(data.get("data", [{}])[0].get("attributes").get("positionId")),
+        "position_2": POSITION_MAP.get(data.get("data", [{}])[0].get("attributes").get("firstSidePositionId")),
+        "position_3": POSITION_MAP.get(data.get("data", [{}])[0].get("attributes").get("secondSidePositionId")),
+        "former_clubs": data.get("data", [{}])[0].get("attributes").get("formerClubsNote"),
+        "market_value": data.get("data", [{}])[0].get("marketValueDetails").get("highest").get("value"),
+        "prime_date": data.get("data", [{}])[0].get("marketValueDetails").get("highest").get("determined")
     }
---
+++ scripts/process/4_players/parsers/stats_parser.py
@@ -0,0 +1,51 @@
+import json
+
+from mappings.positions import POSITION_MAP
+
+
+def parse_stats(stats_path):
+    with open(stats_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    matches = []
+
+    for item in data["data"]["performance"]:
+
+        general = item["statistics"]["generalStatistics"]
+        goals = item["statistics"]["goalStatistics"]
+        play = item["statistics"]["playingTimeStatistics"]
+
+        minutes = play.get("playedMinutes") or 0
+
+        position_id = general.get("positionId")
+
+        matches.append({
+            "game_id": item["gameInformation"]["gameId"],
+            "season": item["gameInformation"]["season"]["display"],
+            "competition_id": item["gameInformation"]["competitionId"],
+            "date": item["gameInformation"]["date"]["dateTimeUTC"],
+
+            "club_id": item["clubsInformation"]["club"]["clubId"],
+            "opponent_id": item["clubsInformation"]["opponent"]["clubId"],
+
+            "position": POSITION_MAP.get(position_id),
+
+            "minutes": minutes,
+            "started": play.get("isStarting", False),
+
+            "captain": general.get("isCaptain", False),
+
+            "goals": goals.get("goalsScoredTotal") or 0,
+            "assists": goals.get("assists") or 0,
+
+            "goals_conceded":
+                goals.get("opponentGoalsOnThePitch") or 0,
+
+            "participation":
+                general.get("participationState"),
+
+            "club_points":
+                item["clubsInformation"]["club"]["points"]
+        })
+
+    return matches
