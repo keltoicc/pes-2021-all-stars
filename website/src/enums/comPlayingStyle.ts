2315a907d72a2ddb90fa96e33695341b71d20bdc
Keltoi Conde Cabaleiro <keltoi.conde@quobis.com>
2026-08-10 15:11:20 +0200

    Implemented COM Playing Styles

---
+++ website/src/enums/comPlayingStyle.ts
@@ -0,0 +1,9 @@
+export enum ComPlayingStyles {
+    TRICKSTER = "Trickster",
+    MAZINGRUN = "Mazing Run",
+    SPEEDINGBULLET = "Speeding Bullet",
+    INCISIVERUN = "Incisive Run",
+    LONGBALLEXPERT = "Long Ball Expert",
+    EARLYCROSS = "Early Cross",
+    LONGRANGER = "Long Ranger"
+}
--- website/src/types/playerVersion.ts
+++ website/src/types/playerVersion.ts
@@ -2,6 +2,7 @@ import type { Position } from "../enums/position";
 import type { Positions } from "./positions";
 import type { Abilities } from "./abilities";
 import type { PlayingSkills } from "../enums/playingSkill";
+import type { ComPlayingStyles } from "../enums/comPlayingStyle";
 
 export interface PlayerVersion {
     // Identity
@@ -24,6 +25,7 @@ export interface PlayerVersion {
     injuryResistance: number;
 
     playingSkills?: PlayingSkills[];
+    comPlayingStyles?: ComPlayingStyles[];
 
     overall?: number;
 
