16805ebd05eee7fe71be5d1baea614c89ba85835
Keltoi Conde Cabaleiro <keltoi.conde@quobis.com>
2026-08-18 17:56:36 +0200

    Deploy website to GitHub Pages

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://keltoicc.github.io',
+    base: '/pes-2021-all-stars',
+});
