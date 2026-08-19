2f366b3b5e546746430560bcd2f0922eec86254d
Keltoi Conde Cabaleiro <keltoi.conde@quobis.com>
2026-08-19 09:27:24 +0200

    Adapt images

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://keltoicc.github.io',
-    base: '/pes-2021-all-stars',
+    base: '/pes-2021-all-stars/',
 });
